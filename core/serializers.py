5fc9c497648f306ce47333f81458da44cb373b49
Junsu Min <sunsuking@gmail.com>
2022-06-25 16:14:38 +0900

    core update not required

--- core/serializers.py
+++ core/serializers.py
@@ -12,3 +12,5 @@ class StatusUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Status
         fields = ['illuminance', 'temperature', 'humidity', 'co2']
+        extra_kwargs = {'illuminance': {'required': False}, 'temperature': {'required': False},
+                        'humidity': {'required': False}, 'co2': {'required': False}}
